f548a8dab261c14a604fb2b8eb994edcd0c2fac1
Diego <diegods1505@gmail.com>
2023-04-29 22:37:15 -0300

    Atualização de Projeto

--- app/src/main/java/com/example/empregaeu4/Tela2Activity.kt
+++ app/src/main/java/com/example/empregaeu4/Tela2Activity.kt
@@ -1,8 +1,18 @@
 package com.example.empregaeu4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.empregaeu4.databinding.ActivityTela2Binding
+
+private infix fun Nothing?.intent(tela2Activity: Tela2Activity): Any {
+
+}
+
+object new {
+
+}
 
 class Tela2Activity : AppCompatActivity() {
 
@@ -17,5 +27,12 @@ class Tela2Activity : AppCompatActivity() {
         val nome = intent.getStringExtra("nome")
         usuarioNome.text = nome
         //setContentView(R.layout.activity_tela2)
+
+    }
+
+    fun chamarAlimentacao(view: View) {
+
+        intent = new intent (this, Activity_tela_3.class);
+        startActivity(intent)
     }
 }
